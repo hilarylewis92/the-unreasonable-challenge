247cbf4090e9223779db5398bbd442a5bf579e27
Hilary Lewis <hilarylewis92@gmail.com>
2017-01-03 12:54:41 -0700

    Add masonry component to challenge list

--- src/components/ChallengesList.js
+++ src/components/ChallengesList.js
@@ -1,26 +1,46 @@
 import React, { Component } from 'react'
+import Masonry from 'react-masonry-component'
 
 
-import ChallengeCard from './ChallengeCard'
+var masonryOptions = {
+  transitionDuration: 0
+}
 
 export default class ChallengesList extends Component {
 
    render() {
      const{ challengesList } = this.props;
 
+     var childElements = challengesList.map(challenge => {
+       return (
+         <li className = 'challenge-elements'>
+           <div className='single-challenge-title'>
+             {challenge.title}
+           </div>
+
+           <div className='single-challenge-body'>
+             {challenge.body}
+           </div>
+
+           <img className='single-challenge-image'
+             src={challenge.image}
+            />
+         </li>
+       )
+     })
+
      return (
        <div className="ChallengesList">
          <h3>Challenges</h3>
-         <ul>
-           {challengesList.map(challenge => {
-             return (
-               <ChallengeCard
-                 key={challenge.createdAt}
-                 challenge = {challenge}
-                />
-              )
-           })}
-         </ul>
+         <Masonry
+           classname={'my-gallery-class'}
+           elementType={'ul'}
+           options={masonryOptions}
+           disableImagesLoaded={false}
+           updateOnEachImageLoad={false}
+           >
+          {childElements}
+        </Masonry>
        </div>
      )
    }
