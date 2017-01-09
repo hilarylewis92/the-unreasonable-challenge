6b548df1c4c71387121c5452fb9b6033e1cbd7a9
Hilary Lewis <hilarylewis92@gmail.com>
2017-01-09 15:41:07 -0700

    Incomplete and complete challenges in different masonry

--- src/components/ChallengesList.js
+++ src/components/ChallengesList.js
@@ -15,18 +15,16 @@ export default class ChallengesList extends Component {
   render() {
     const{ challengesList, onEditTitle, onEditBody, editChallenge, removeChallenge, toggleCheck, handleImageChange, draftChallengeTitle, draftChallengeBody, imagePreviewURL} = this.props
 
-
-
-    var singleCard = challengesList.map(challenge => {
+    var completedChallenge = challengesList.map(challenge => {
 
       var bodyText = challenge.body.length > 100
         ? challenge.body.slice(0, 100) + ` ...`
         : challenge.body
 
       return (
-
+       challenge.checked ?
         <li
-          className='challenge-elements'
+          className='challenge-elements completed'
           key={challenge.key}>
 
           <CardDisplay
@@ -63,6 +61,59 @@ export default class ChallengesList extends Component {
 
           </ul>
         </li>
+        :
+        null
+      )
+    })
+
+    var inCompleteChallenge = challengesList.map(challenge => {
+
+      var bodyText = challenge.body.length > 100
+        ? challenge.body.slice(0, 100) + ` ...`
+        : challenge.body
+
+      return (
+       !challenge.checked ?
+        <li
+          className='challenge-elements in-complete'
+          key={challenge.key}>
+
+          <CardDisplay
+            challenge={challenge}
+          />
+
+          <div
+            className='single-challenge-body'
+            aria-label='challenge body'>
+            {bodyText}
+          </div>
+
+          <ul>
+           <ChallengeCardModal
+             challenge={challenge}
+            />
+
+            <EditFormModal
+              challenge={challenge}
+              editChallenge={editChallenge}
+              removeChallenge={removeChallenge}
+              handleImageChange={handleImageChange}
+              onEditTitle={onEditTitle}
+              draftChallengeTitle={draftChallengeTitle}
+              onEditBody={onEditBody}
+              draftChallengeBody={draftChallengeBody}
+              imagePreviewURL={imagePreviewURL}
+            />
+
+            <CompleteChallenge
+              challenge={challenge}
+              toggleCheck={toggleCheck}
+            />
+
+          </ul>
+        </li>
+        :
+        null
       )
     })
 
@@ -77,7 +128,17 @@ export default class ChallengesList extends Component {
           disableImagesLoaded={false}
           updateOnEachImageLoad={false}
         >
-          {singleCard}
+          {completedChallenge}
+        </Masonry>
+
+        <Masonry
+          className={''}
+          elementType={'ul'}
+          options={masonryOptions}
+          disableImagesLoaded={false}
+          updateOnEachImageLoad={false}
+        >
+          {inCompleteChallenge}
         </Masonry>
 
        </div>
